7bd711aaed1ad7891e3b6bb9ec253c3645607db4
Caroline <125596928+C4rolcamargo@users.noreply.github.com>
2024-09-10 13:07:12 -0300

    Create Salvos.aspx.cs

---
+++ 2024-08-20Final1/Salvos.aspx.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2024._08._27
+{
+    public partial class salvos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
